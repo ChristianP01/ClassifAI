18d6068df3a1eb47e032178b50a16a78288a3f1c
Michele Bianco <bianco.michele@outlook.com>
2024-02-21 17:04:08 +0100

    Final fix DataframeCleaner (I hope)
    Co-Authored-By: Preti Christian <50235687+ChristianP01@users.noreply.github.com>

--- src/main/scala/data/DataframeCleaner.scala
+++ src/main/scala/data/DataframeCleaner.scala
@@ -3,7 +3,8 @@ package data
 import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher}
 import com.johnsnowlabs.nlp.annotator.{Stemmer, Tokenizer}
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.sql.functions.{not, regexp_replace}
+import org.apache.spark.sql.functions.{col, explode, monotonically_increasing_id, not, regexp_replace, row_number, when}
+import org.apache.spark.sql.expressions.Window
 import org.apache.spark.ml.feature.StopWordsRemover
 import org.apache.spark.sql.types.{IntegerType, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
@@ -11,10 +12,10 @@ import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 class DataframeCleaner(private val spark: SparkSession, private var df: DataFrame) {
   /** Get the transformed dataframe */
   def getDataFrame: DataFrame = {
-    df
+    this.df
   }
 
-  println("Starting preprocessing phase 1")
+  println("Start preprocessing")
 
   /** Remove rows with not allowed topics */
   private val topics = List(
@@ -32,37 +33,34 @@ class DataframeCleaner(private val spark: SparkSession, private var df: DataFram
   )
 
   /** Remove rows with wrong topic (parsing fault) */
-  this.df = df.filter(df.col("Context/Topic").isin(topics: _*))
+  this.df = this.df.filter(this.df.col("Context/Topic").isin(topics: _*))
 
   // Remove rows with non-English sentences
-  private val notAlphDF = df.filter(not(df.col("Text").rlike("[a-zA-Z]")))
-  private val accentsDF = df.filter(df.col("Text").rlike("[àáâãäåçèéêëìíîïòóôõöùúûü]"))
+  private val notAlphDF = this.df.filter(not(this.df.col("Text").rlike("[a-zA-Z]")))
+  private val accentsDF = this.df.filter(this.df.col("Text").rlike("[àáâãäåçèéêëìíîïòóôõöùúûü]"))
 
   this.df = this.df.except(notAlphDF.unionByName(accentsDF))
 
   /** Remove punctuations and multiple whitespace */
-  this.df = df
-    .withColumn("NoSymbols", regexp_replace(df.col("Text"), "[^a-zA-Z\\s]", ""))
+  this.df = this.df
+    .withColumn("NoSymbols", regexp_replace(this.df.col("Text"), "[^a-zA-Z\\s]", ""))
     .drop("Text")
-
-  this.df = df
-    .withColumn("NoPunct", regexp_replace(df.col("NoSymbols"), "\\s+", " "))
-    .drop("NoSymbols")
+  this.df = this.df.withColumn("NoPunct", regexp_replace(this.df.col("NoSymbols"), "\\s+", " "))
 
   /** Create document assembler to work with stemmer */
   private val documentAssembler = new DocumentAssembler()
     .setInputCol("NoPunct")
-    .setOutputCol("document")
+    .setOutputCol("Document")
 
   /** Split the text in array of tokens */
   private val tokenizer = new Tokenizer()
     .setInputCols(Array(documentAssembler.getOutputCol))
-    .setOutputCol("tokens")
+    .setOutputCol("Tokens")
 
   /** Calculate words' stem */
   private val stemmer = new Stemmer()
     .setInputCols(Array(tokenizer.getOutputCol))
-    .setOutputCol("stem")
+    .setOutputCol("Stem")
     .setLanguage("English")
 
   private val finisher = new Finisher()
@@ -79,65 +77,45 @@ class DataframeCleaner(private val spark: SparkSession, private var df: DataFram
 
   this.df = pipeline.fit(df).transform(df)
 
-  println("Ended preprocessing phase 1")
-  println("Starting preprocessing phase 2")
+  println("End preprocessing")
+  println("Start pivoting")
 
-  var wordList: List[String] = List.empty // List of words for columns-schema
-  var sentenceList: List[Seq[String]] = List.empty // List of sentences to fill cells
+  private val maxPivots = 30000
+  /** Increase max pivot in a dataframe to maxPivots */
+  spark.conf.set("spark.sql.pivotMaxValues", maxPivots)
 
-  for (row <- this.df.select("Text").rdd.collect()) {
+  /** Add an index column for every sentence */
+  this.df = df.withColumn("Index", row_number().over(Window.orderBy(monotonically_increasing_id())))
 
-    val sentence = row.getSeq[String](0).toList
-    sentenceList :+= sentence
+  /** Split a single sentence in multiple rows */
+  this.df = this.df.select(this.df.col("Index"), this.df.col("Context/Topic"), explode(this.df.col("Text")).as("Word"))
 
-    sentence.foreach( word => {
-      if (!wordList.contains(word)) {
-        wordList :+= word
-      }
-    })
-  }
+  /** Remove blank characters */
+  this.df = this.df.filter(!this.df.col("Word").rlike("^\\s*$"))
 
-  var schema = new StructType()
+  /** Remove sparse words with count less than N */
+  private var wordCounts = this.df.groupBy("Word").count()
+  private val N = 2
+  wordCounts = wordCounts.filter(wordCounts.col("count") >= N)
+  this.df = this.df.join(wordCounts, "Word")
 
-  wordList.foreach(fieldName => {
-    schema = schema.add(fieldName, IntegerType)
-  })
-
-  // Edit since, for each sentence in sentenceList, 1 if word is in else 0
-  var cells: Array[Row] = Array.empty
-  var tmpSeq: Array[Any] = Array.empty
-
-  sentenceList.foreach(sentence => {
-    tmpSeq = Array.fill(wordList.length)(0)
-
-    sentence.foreach(word => {
-      tmpSeq(wordList.indexOf(word)) = 1
-    })
-    cells = cells :+ Row.fromSeq(tmpSeq)
-  })
-
-  println("Ended preprocessing phase 2")
-  println("Saving new preprocessed dataframe")
-
-  val newdf: DataFrame = this.spark.createDataFrame(spark.sparkContext.parallelize(cells), schema)
-
-  /**
-  /**
-    * For each word in each row of the "old" DataFrame, create a new row with relative count-per-topic.
-    */
-  private val topicWordCount = df
-    .select(this.df.col("Context/Topic"), explode(this.df.col("Text")).as("Word"))
-    .groupBy("Word", "Context/Topic")
+  /** Pivot the word column and make a count of words occurrences for every sentences */
+  this.df = this.df
+    .groupBy("Index", "Context/Topic")
+    .pivot("Word")
     .count()
+    .drop("Index") //Remove Index column
+    .na.fill(0) //Fill NULL values with 0
 
-  /** Pivoting dataset with topics as cols */
-  private val pivoted = topicWordCount.groupBy("Word").pivot("Context/Topic").sum("count")
+  println("End pivoting")
+  println("Start saving")
 
-  this.df = topics.foldLeft(pivoted) { (data, column) =>
-    data.withColumn(column, coalesce(col(column), lit(0)))
-  }
+  this.df
+    .write
+    .option("header",value = true)
+    .format("csv")
+    .mode("overwrite")
+    .save(System.getProperty("user.dir") + "/output/")
 
-  /** Add a column with sum of a word's occurrences */
-  this.df = df.withColumn("Total", df.columns.drop(1).map(df(_)).reduce(_ + _))
-  */
+  println("End saving")
 }
