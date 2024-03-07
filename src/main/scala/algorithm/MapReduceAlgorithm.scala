7b6cc25b8d48c3166742e932cb13afafce6e15c5
Michele Bianco <bianco.michele@outlook.com>
2024-03-07 19:09:09 +0100

    Fix attrTable
    Co-Authored-By: Preti Christian <50235687+ChristianP01@users.noreply.github.com>

--- src/main/scala/algorithm/MapReduceAlgorithm.scala
+++ src/main/scala/algorithm/MapReduceAlgorithm.scala
@@ -3,8 +3,6 @@ package algorithm
 import org.apache.spark.sql.{DataFrame, Row}
 
 class MapReduceAlgorithm {
-  /** a, row_id, c */
-  private var attrTable: Seq[(String, Int, String)] = Seq.empty
   /** a, c, cnt */
   val countTable: Map[String, (String, Int)] = Map.empty
   /** node_id (hash), row_id */
@@ -17,16 +15,18 @@ class MapReduceAlgorithm {
    */
   def dataPreparation(df: DataFrame): Unit = {
     /** Populate attrTable */
-    attrTable = df.collect().flatMap { row => {
+    val attrTable = df.rdd.flatMap { row =>
       row
         .toSeq
         .zipWithIndex
         .slice(2, row.toSeq.length)
         .filter(_._1 == "1")
-        .map { elem =>
-          (df.columns(elem._2), row(0).toString.toInt, row(1).toString)
+        .map {
+          case (_, idx) =>
+          (row.schema.fieldNames(idx), (row(0).toString.toInt, row(1).toString))
         }
-      }
     }
+
+    println(attrTable.collect().mkString("Array(", ", ", ")"))
   }
 }
